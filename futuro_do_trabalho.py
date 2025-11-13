b9bb1e03d3b0a67bfd0176feb4710f3f57b5586d
Joao Paulo <bpjoao00@gmail.com>
2025-11-13 19:14:13 -0300

    montando estrutura inicial e colocando nome dos participantes

---
+++ futuro_do_trabalho.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+def obter_tendencias_vagas ():
+
+
+
+
+def filtrar_vagas(lista_vagas):
+
+
+
+
+
+
+def calcular_crescimento_total(profissoes):
+
+
+
+
+
+def exibir_profissoes(lista_profissoes):
+
+
+
+
+
+def main ():
+
+
+
+
+if __name__ == '__main__':
+    main()
