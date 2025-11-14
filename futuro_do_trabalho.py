061f8518e8511bc4a94d4f84a4cd52b942b3546d
Joao Paulo <bpjoao00@gmail.com>
2025-11-14 11:35:06 -0300

    fazendo requisicao da API e criando arquivo de dependencias

--- futuro_do_trabalho.py
+++ futuro_do_trabalho.py
@@ -1,5 +1,29 @@
 import requests
 import json
+from dotenv import load_dotenv
+import http.client
+import os
+
+load_dotenv()
+
+conn = http.client.HTTPSConnection("jsearch.p.rapidapi.com")
+
+API_KEY = os.getenv('rapidapi-key')
+API_HOST = os.getenv('rapidapi-host')
+
+headers = {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": API_HOST
+}
+
+
+conn.request("GET", "/search?query=developer%20jobs%20in%20chicago&page=1&num_pages=1&country=us&date_posted=all", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+
 
 def obter_tendencias_vagas ():
 
